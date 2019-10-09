20c71900e429b3121b2d64043aaea05d9b80c9c1
Patrick Goldinger <patrick.goldinger.02@gmail.com>
2019-10-09 20:09:43 +0200

    Added Keyboard support / Help dialog. [v0.4.0]

--- scripts/main.js
+++ scripts/main.js
@@ -16,4 +16,20 @@ window.onload = function () {
     document.getElementById("tttctrl__switch").addEventListener("click", function (e) {
         ttt.startGame(ttt._game.mode, true, true);
     }, false);
+    
+    document.body.addEventListener("keypress", function (e) {
+        let showHelpDialogEle = document.getElementById("show-help-dialog");
+        if (["1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(e.key) && !showHelpDialogEle.checked) {
+            ttt.handleClick({
+                target: document.getElementById("tttf__" + e.key),
+            }, true);
+            e.preventDefault();
+        } else if (e.key == "?") {
+            showHelpDialogEle.checked = !showHelpDialogEle.checked;
+        } else if (e.key == "r" && !showHelpDialogEle.checked) {
+            ttt.restart();
+        } else if ((e.key == "0" || e.key == "o") && !showHelpDialogEle.checked) {
+            ttt.startGame(ttt._game.mode, true, true);
+        }
+    }, false);
 };
